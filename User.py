86e076172ba6dfb896d584f7932b54fbff383d4a
Ben Zuke <benjaminzuke@gmail.com>
2023-01-28 12:28:48 -0500

    user baseclass

--- User.py
+++ User.py
@@ -6,8 +6,38 @@ Methods: login, logout, register, edit_profile, delete_account.
 
 class User:
     def __init__(self) -> None:
-        self.name = ""
-        self.email = ""
-        self.password = ""
+        self.user_id = 0
+        self.name = "bob jones"
+        self.email = "benzuke34@gmail.com"
+        self.password = "pass"
+        self.payment_info = "1234567890"
         self.budget = 0
         self.expenses = []
+
+    '''
+    Login to the users account.
+    '''
+    def login(self):
+        pass
+
+    '''
+    Logout of the users account.
+    '''
+    def logout(self):
+        pass
+
+    '''
+    Register a new user.
+    '''
+    def register(self):
+        pass
+
+
+    def edit_profile(self):
+        pass
+
+    '''
+    Delete the users account.
+    '''
+    def delete_account(self):
+        pass
